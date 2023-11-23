9b14f37e56e702c7bb9aad0aef3ed335a965cb29
Peladinn <gonzfran1812@gmail.com>
2023-11-22 21:39:38 -0300

    terminado

---
+++ eje1.js
@@ -0,0 +1,12 @@
+function gritar(grito) {
+    return '¡' + grito + '!';
+}
+gritar();
+
+console.log ( gritar ('BOCAAAA') );
+console.log ( gritar ('METIOOOO UN') );
+console.log ( gritar ('GO') );
+console.log ( gritar ('LA') );
+console.log ( gritar ('ZOOOOOOOO') );
+
+
